b08ada6fd19735f8a5928bf1293b1a2a47b9b897
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-10-07 00:40:59 +0800

    Updates

--- assets/index-48f2a4eb.js
+++ assets/index-48f2a4eb.js
@@ -271,7 +271,9 @@ tags: [tools]
 
 * [Vue Tutorial](https://vuejs.org/guide/essentials/list.html)
 
-* [Fontawesome Usage in Latex](https://blog.csdn.net/zzq060143/article/details/89380160), CSDN`}],Xl={"How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023":[{tags:["vue3","github page"],path:"notes/How to Deploy Your Vite:Vue 3 Application in GitHub Pages 2023.md",content:`---
+* [Fontawesome Usage in Latex](https://blog.csdn.net/zzq060143/article/details/89380160), CSDN
+
+* [Subtitles Process](https://www.iflyrec.com/), cheapest and best results.`}],Xl={"How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023":[{tags:["vue3","github page"],path:"notes/How to Deploy Your Vite:Vue 3 Application in GitHub Pages 2023.md",content:`---
 title: "How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023"
 tags: [vue3, github page]
 ---
@@ -651,7 +653,9 @@ tags: [tools]
 
 * [Vue Tutorial](https://vuejs.org/guide/essentials/list.html)
 
-* [Fontawesome Usage in Latex](https://blog.csdn.net/zzq060143/article/details/89380160), CSDN`}],Og={vue3:Cg,"github page":[{title:"How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023",tags:["vue3","github page"],path:"notes/How to Deploy Your Vite:Vue 3 Application in GitHub Pages 2023.md",content:`---
+* [Fontawesome Usage in Latex](https://blog.csdn.net/zzq060143/article/details/89380160), CSDN
+
+* [Subtitles Process](https://www.iflyrec.com/), cheapest and best results.`}],Og={vue3:Cg,"github page":[{title:"How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023",tags:["vue3","github page"],path:"notes/How to Deploy Your Vite:Vue 3 Application in GitHub Pages 2023.md",content:`---
 title: "How to Deploy Your Vite/Vue 3 Application in GitHub Pages 2023"
 tags: [vue3, github page]
 ---
